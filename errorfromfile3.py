8d55e4c2c16992b8def3bcdc35bbe1bf23cd4bb1
sylvmt <57760276+sylvmt@users.noreply.github.com>
2019-12-16 22:45:48 -0500

    Update errorfromfile3.py

--- errorfromfile3.py
+++ errorfromfile3.py
@@ -6,8 +6,7 @@ import numpy as np
 import warnings
 warnings.simplefilter("ignore", DeprecationWarning)
 
-dummy, tempo = sys.argv
-print("tempo: " + str(tempo)) #specify for quarter note bpm
+dummy = sys.argv
 
 samplerate = 44100 #samples per second, standard for audio files
 buffer_size = 1024 #smaller buffer size so that processing time will be every short
@@ -15,11 +14,10 @@ py_format = pyaudio.paInt32 #large file size but better processing
 channels = 1
 total_frames = 0
 tolerance = 0.3
-expected = [261.63, 293.67, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 493.88, 440.00, 392.00] 
+expected = [(261.63, 2.5), (293.67, 2.5), (329.63, 2.5), (349.23, 2.5), (392.00, 2.5), (440.00, 2.5), (493.88, 2.5), (523.25, 2.5), (493.88, 2.5), (440.00, 2.5), (392.00, 2.5)] 
 
 temp = "C-1"
 time_s = 0
-quarter = 60/int(tempo)
 times = []
 pitches = []
 
@@ -39,6 +37,8 @@ pitchd.set_unit("Hz")
 pitchd.set_tolerance(tolerance)
 
 notes = 0
+pnotes = 0
+tleast = 0
 error = 0
 nt = 0
 
@@ -54,35 +54,43 @@ while True:
         seconds = total_frames/float(samplerate) # equals seconds passed
         if note != temp:
             if nt == 0 or note != "C-1":
-                #CALC PRINT LENGTH AND TIME OF CURRENT NOTE
+                #CALC PRINT LENGTH AND TIME OF CURRENT NOTE AS WELL AS TEMPO/TIME ERROR
                 nt = nt + 1
-                time_t = seconds - time_s
-                time_s = seconds
-                typen = time_t/quarter
-                temp = note
-                pitches += [(pitch, note, time_t)]
-                print(str(note))
-                print("length: " + str(time_t) + " pitch: " + str(pitch))
 
-                #CALC PRINT CURRENT ERROR AND NOTE NUMBER
+                #print("pnotes:" + str(pnotes) + " ... notes:" + str(notes))
+                if pnotes != notes:
+                    time_t = seconds - time_s
+                    time_s = seconds
+                    print(time_t)
+                    tleast = abs(expected[notes][1] - time_t)
+                    #print("tleast: " + str(tleast))
+                pnotes = notes
+
+                #temp = note
+                #pitches += [(pitch, note, time_t)]
+                print(str(note))
+                #print("length: " + str(time_t) + " pitch: " + str(pitch))
+
+                #CALC PRINT CURRENT (PITCH) ERROR AND NOTE NUMBER
                 harmonic1 = pitch/2
                 harmonic2 = pitch*2
-                least1 = (min(abs(expected[notes] - pitch), abs(expected[notes-1] - pitch), abs(expected[notes+1] - pitch)))
-                least2 = (min(abs(expected[notes] - harmonic1), abs(expected[notes-1] - harmonic1), abs(expected[notes+1] - harmonic1)))
-                least3 = (min(abs(expected[notes] - harmonic2), abs(expected[notes-1] - harmonic2), abs(expected[notes+1] - harmonic2)))
+                least1 = (min(abs(expected[notes][0] - pitch), abs(expected[notes-1][0] - pitch), abs(expected[notes+1][0] - pitch)))
+                least2 = (min(abs(expected[notes][0] - harmonic1), abs(expected[notes-1][0] - harmonic1), abs(expected[notes+1][0] - harmonic1)))
+                least3 = (min(abs(expected[notes][0] - harmonic2), abs(expected[notes-1][0] - harmonic2), abs(expected[notes+1][0] - harmonic2)))
                 least = min(least1, least2, least3)
                 #print("leasts: " + str(least1) + " " + str(least2) + " " + str(least3))
                 #print("w: " + str(abs(expected[notes-1] - harmonic1)))
+
                 print("current error: " + str(least))
-                if least == abs(expected[notes] - pitch) or least == abs(expected[notes] - harmonic1) or  least == abs(expected[notes] - harmonic2):
+                if least == abs(expected[notes][0] - pitch) or least == abs(expected[notes][0] - harmonic1) or  least == abs(expected[notes][0] - harmonic2):
                     notes = notes + 1
-                elif least == abs(expected[notes-1] - pitch) or  least == abs(expected[notes-1] - harmonic1) or  least == abs(expected[notes-1] - harmonic2):
+                elif least == abs(expected[notes-1][0] - pitch) or  least == abs(expected[notes-1][0] - harmonic1) or  least == abs(expected[notes-1][0] - harmonic2):
                     notes = notes
-                elif least == abs(expected[notes+1] - pitch) or  least == abs(expected[notes-1] - harmonic2) or  least == abs(expected[notes-1] - harmonic2):
+                elif least == abs(expected[notes+1][0] - pitch) or  least == abs(expected[notes-1][0]- harmonic2) or  least == abs(expected[notes-1][0] - harmonic2):
                     notes = notes + 2
 
                 #CALC PRINT TOTAL ERROR
-                error = error + least
+                error = error + least*0.7 + tleast*0.3
                 print("total error: " + str(error))
                 print("note #" + str(notes))
 
