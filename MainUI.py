107b6179ef25b38b89f51413becec0dab473ad3b
alexfsong <37254730+alexfsong@users.noreply.github.com>
2019-12-16 22:49:12 -0500

    Update MainUI.py

--- MainUI.py
+++ MainUI.py
@@ -1,9 +1,13 @@
 from RunAudiveris import *
 from FileExplorer import *
 def main():
-   fileexplorer = FileExplorer()
+   root = Tk()
+   fileexplorer = FileExplorer(root)
    audiveris = RunAudiveris(fileexplorer.get_inputfilepath(),fileexplorer.get_outputfilepath())
-   #print(audiveris.makemxl())
+   audiveris.makexml()
+   filename = fileexplorer.get_inputfilepath()[fileexplorer.get_inputfilepath().rfind('/'):fileexplorer.get_inputfilepath().rfind('.')]
+   xmlfilepath = fileexplorer.get_outputfilepath() + filename + '/' + filename + '.xml'
+   notes_durations = XMLParser.parseXML(xmlfilepath)
 
 if __name__ == '__main__':
    main()
